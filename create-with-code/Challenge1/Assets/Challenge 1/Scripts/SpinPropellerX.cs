f0e254245e9b9102c6ab7a615d38612eea0972ac
Ryan Lewin <lewin.ry+development@gmail.com>
2020-06-18 11:27:41 +1000

    Finished Challenge 1

---
+++ create-with-code/Challenge1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private Vector3 rotations = new Vector3(0, 0, 1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotations, speed);
+    }
+}
