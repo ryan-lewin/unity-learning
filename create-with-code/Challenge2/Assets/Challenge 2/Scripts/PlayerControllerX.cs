5c959514be081ef14fcf490a9ce5f0fc10ed45f0
Ryan Lewin <lewin.ry+development@gmail.com>
2020-06-23 11:30:22 +1000

    Finished Challenge 2

---
+++ create-with-code/Challenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float cooldown = 1.5f;
+    private float timeSince = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            float current = Time.time;
+
+            if(current - timeSince >= cooldown)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                timeSince = current;
+            }
+        }
+    }
+}
